a4d64a8bea5997f875c317ad87fed2de08ab236d
TrilhaFront <trilhafront@gmail.com>
2023-06-13 12:55:18 -0300

    video1

---
+++ src/agenda/Principal.java
@@ -0,0 +1,18 @@
+package agenda;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Bem vindo a Agenda de Contatos!");
+		System.out.println("");
+		System.out.println("1. Cadastrar Contato");
+		System.out.println("2. Listar Contatos");
+		System.out.println("3. Editar Contato");
+		System.out.println("4. Excluir Contato");
+		System.out.println("9. Sair");
+		System.out.println("");
+		
+		System.out.print("Opção: ");
+	}
+
+}
